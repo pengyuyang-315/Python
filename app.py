f76446cde2236f036a64361a5f86b692b2f15f37
pengyuyang-315 <81129760+pengyuyang-315@users.noreply.github.com>
2022-08-03 19:49:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,153 +1,50 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "6f585677",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from flask import Flask, request, render_template"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "427faca6",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import joblib"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "14898c96",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "app=Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "id": "fb99e4ed",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route(\"/\",methods=[\"GET\",\"POST\"])\n",
-    "def index():\n",
-    "    if request.method==\"POST\":\n",
-    "        rates=float(request.form.get(\"num\"))\n",
-    "        print(rates)\n",
-    "        model1=joblib.load(\"regression\")\n",
-    "        r1=model1.predict([[rates]])\n",
-    "        model2 = joblib.load(\"tree\")\n",
-    "        r2 = model2.predict([[rates]])\n",
-    "        return (render_template(\"index.html\",result1=r1,result2=r2))\n",
-    "    else:\n",
-    "        return(render_template(\"index.html\",result1=\"waiting\",result2=\"waiting\"))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a8b604e5",
-   "metadata": {
-    "scrolled": true
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "   WARNING: This is a development server. Do not use it in a production deployment.\n",
-      "   Use a production WSGI server instead.\n",
-      " * Debug mode: off\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
-      "127.0.0.1 - - [03/Aug/2022 18:32:22] \"\u001b[37mGET / HTTP/1.1\u001b[0m\" 200 -\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "0.01\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "127.0.0.1 - - [03/Aug/2022 18:32:26] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n",
-      "127.0.0.1 - - [03/Aug/2022 18:33:07] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "1.38\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "127.0.0.1 - - [03/Aug/2022 19:09:36] \"\u001b[37mPOST / HTTP/1.1\u001b[0m\" 200 -\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "1.4\n"
-     ]
-    }
-   ],
-   "source": [
-    "if __name__==\"__main__\":\n",
-    "    app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "271ba6d3",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[2]:
+
+
+import joblib
+
+
+# In[3]:
+
+
+app=Flask(__name__)
+
+
+# In[4]:
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        rates=float(request.form.get("num"))
+        print(rates)
+        model1=joblib.load("regression")
+        r1=model1.predict([[rates]])
+        model2 = joblib.load("tree")
+        r2 = model2.predict([[rates]])
+        return (render_template("index.html",result1=r1,result2=r2))
+    else:
+        return(render_template("index.html",result1="waiting",result2="waiting"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
